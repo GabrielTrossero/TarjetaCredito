f2824fd0ea756f19f19ae05d6affbf8952ee3173
Gabriel Trossero <gabi_trossero@hotmail.com>
2022-10-05 12:51:14 -0300

    configuracion y generacion de migracion

---
+++ Backend/Backend/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend
+{
+    //DbContext lo que hace es crear una instancia de nuestra BD para:
+    //Almacenar datos, realizar querys, crear la BD a partir del modelo, etc
+    public class AplicationDbContext: DbContext
+    {
+        //Para mapear nuestros modelos con las tablas de la BD
+        DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
